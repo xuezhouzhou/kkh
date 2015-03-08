9a71cbefadee045cde4965484db3d059607bc8e8
xuezhouzhou <xzzphp@163.com>
2015-03-08 16:56:31 +0800

    修改密码页面，其他细节调整

---
+++ js/common.js
@@ -0,0 +1,14 @@
+(function($){
+  //右上角账号信息下拉菜单
+  $(".login-info").on('mouseenter',function(){
+    $('.login-info-list').show(); 
+  }).on('mouseleave',function(){
+    $('.login-info-list').hide();
+  });
+
+  //互动管理二级菜单
+  $("#side-menu-hdgl").on('click',function(){
+    $(this).find('.icon-jt').toggleClass('icon-jt-bottom');
+    $("#hdgl-child").slideToggle();
+  });
+})(jQuery);  
