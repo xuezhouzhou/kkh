942e07ce5325e60958414040a2838f792a3b3854
xuezhouzhou <xzzphp@163.com>
2015-03-30 16:35:25 +0800

    首页 我们的服务模块 交互添加

--- front/js/index.js
+++ front/js/index.js
@@ -1,6 +1,9 @@
 (function($){
   var timer1 ;
 
+  //我们的服务是否有展开
+  var serviceOpen = 0;
+
   //案例类型 切换
   $(".tab a").on("click",function(){
     var dataID = $(this).attr('data-id');
@@ -31,6 +34,8 @@
     $_this.find('.info').animate({top: 310},300);
   });
 
+
+  //场地左按钮
   $(".site-scroll-left").on('click',function(){
     $_this = $(this);
     var index = $_this.siblings('.index').val();
@@ -38,7 +43,8 @@
     if(index<=0) index = 5;
     siteScroll($_this,index);
   });
-
+  
+  //场地右边按钮
   $(".site-scroll-right").on('click',function(){
     $_this = $(this);
     var index = $_this.siblings('.index').val();
@@ -46,6 +52,74 @@
     if(index>5) index = 1;
     siteScroll($_this,index);
   });
+
+  placeHolder($('.city'),'活动城市');
+  placeHolder($('.type'),'会议类型');
+  placeHolder($('.time'),'会议时长');
+  placeHolder($('.total'),'参加人数');
+  placeHolder($('.spending'),'会议预算');
+
+  //炫酷互动
+  $('.service .interactive .icon').on('click',function(){
+    var $_this = $(this);
+    serviceAni($_this);
+    $('.service .fast').animate({'left':'75%'},300);
+    $('.service .consumption').animate({'left':'100%'},300);
+    $('.service .analysis').animate({'left':'125%'},300); 
+  });
+  
+  //极速互联
+  $('.service .fast .icon').on('click',function(){
+    var $_this = $(this);
+    serviceAni($_this);
+    $('.service .interactive').animate({'left':'-25%'},300);
+    $('.service .fast').animate({'left':'0'},300);
+    $('.service .consumption').animate({'left':'75%'},300);
+    $('.service .analysis').animate({'left':'100%'},300); 
+  });
+
+  //会议消费
+  $('.service .consumption .icon').on('click',function(){
+    var $_this = $(this);
+    serviceAni($_this);
+    $('.service .interactive').animate({'left':'-50%'},300);
+    $('.service .fast').animate({'left':'-25%'},300);
+    $('.service .consumption').animate({'left':'0'},300);
+    $('.service .analysis').animate({'left':'75%'},300); 
+  });
+  
+  //智能分析
+  $('.service .analysis .icon').on('click',function(){
+    var $_this = $(this);
+    serviceAni($_this);
+    $('.service .interactive').animate({'left':'-50%'},300);
+    $('.service .fast').animate({'left':'-25%'},300);
+    $('.service .consumption').animate({'left':'0'},300);
+    $('.service .analysis').animate({'left':'25%'},300); 
+  });
+
+  function serviceAni($_obj){
+    $('.service .icon').removeClass('icon-cur');
+    $('.service .panel').hide();
+    $('.service .icon').siblings('p').show();
+    $('.service .icon').siblings('h3').show();
+    $_obj.addClass('icon-cur');
+    $_obj.siblings('p').hide();
+    $_obj.siblings('h3').hide();
+    $_obj.siblings('.panel').fadeIn(300);
+    serviceOpen = 1;
+  }
+
+  function serviceReset(){
+    $('.service .icon').removeClass('icon-cur');
+    $('.service .panel').hide();
+    $('.service .icon').siblings('p').show();
+    $('.service .icon').siblings('h3').show();
+    $('.service .interactive').animate({'left':''},300);
+    $('.service .fast').animate({'left':'25%'},300);
+    $('.service .consumption').animate({'left':'50%'},300);
+    $('.service .analysis').animate({'left':'75%'},300);
+  }
 
   function siteScroll($obj,index){
     $obj.siblings('.index').val(index);
@@ -62,9 +136,15 @@
     });      
   }
 
-  placeHolder($('.city'),'活动城市');
-  placeHolder($('.type'),'会议类型');
-  placeHolder($('.time'),'会议时长');
-  placeHolder($('.total'),'参加人数');
-  placeHolder($('.spending'),'会议预算');
+  //关闭 我们的服务 的展开项
+  $("body").on('click',function(e){
+    //判断我们的服务是否有展开
+    if(serviceOpen == 1) {
+      if($(e.target).hasClass('icon')){
+        return;
+      }else{
+        serviceReset();
+      }
+    }
+  });
 })(jQuery); 
